3fac66f46146a3cf7aab8d4782fae7f8d442ff7c
wealthydeveloper <idongesitutong@gmail.com>
2018-12-28 14:26:14 +0100

    first commit

---
+++ resources/views/new.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+
+
+@section('content')
+
+    New page for us.
+
+@stop 
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,8 @@
+@extends('layout')
+
+
+@section('content')
+
+<a href="/todos/public/todos">Visit my todos</a>
+
+@stop
